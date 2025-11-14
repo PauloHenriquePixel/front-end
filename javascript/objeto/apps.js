7a5851d1e8a7fc7ad5fd8573b3ac21fe58e52e60
PauloHenriquePixel <paulohenriquealves4lavra@gmail.com>
2025-11-14 19:13:37 -0300

    primeiro

--- javascript/objeto/apps.js
+++ javascript/objeto/apps.js
@@ -3,7 +3,7 @@ function pessoa (nm,ap ,tlf){
     this.apelido = ap
     this.telefone = tlf
 }
-const p1 = new pessoa ("paulo", "Henrique", "98 98777-7810")
+const p1 = new pessoa ("Paulo", "Henrique", "98 98777-7810")
 console.log(p1.nome)
 console.log(p1.apelido)
 console.log(p1.telefone)
@@ -18,3 +18,13 @@ const p2 = new pessoa2 ("Ana", "Castela", "11 1847294-1010" )
 console.log(p2.nome)
 console.log(p2.apelido)
 console.log(p2.telefone)
+
+function pessoa3 (nm,ap ,tlf){
+    this.nome = nm
+    this.apelido = ap 
+    this.telefone = tlf
+}
+const p3 = new pessoa3 ("Mariana", "Oliveira", "21 99876-5432" )
+console.log(p3.nome)
+console.log(p3.apelido)
+console.log(p3.telefone)
