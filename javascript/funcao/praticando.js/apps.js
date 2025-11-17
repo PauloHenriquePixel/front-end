5d8aa4f490af379fba42e473f8a6ea97a61561b1
PauloHenriquePixel <paulohenriquealves4lavra@gmail.com>
2025-11-17 18:06:25 -0300

    ..

--- javascript/funcao/praticando.js/apps.js
+++ javascript/funcao/praticando.js/apps.js
@@ -1,20 +1,26 @@
-// for(i = 1 ; i < 11; i++)
-// { let a = 2
-// console.log(`${i} X ${a} = ${i * a}`)
+// // for(i = 1 ; i < 11; i++)
+// // { let a = 2
+// // console.log(`${i} X ${a} = ${i * a}`)
+// // }
+
+// // let a = 2;
+// // let i = 1
+// // do {
+    
+    
+// //     console.log(`a multi e ${a} X ${i} = ${a * i}`)
+// //     i++
+// // } while(i < 11 )
+
+// let numero = 1
+// let Numero= 2
+// while(numero <= 10) {
+// console.log(`toma sua multi aki cadela ${numero} X ${Numero} = ${numero * Numero}`)
+//  numero++
 // }
 
-// let a = 2;
-// let i = 1
-// do {
-    
-    
-//     console.log(`a multi e ${a} X ${i} = ${a * i}`)
-//     i++
-// } while(i < 11 )
-
-let numero = 1
-let Numero= 2
-while(numero <= 10) {
-console.log(`toma sua multi aki cadela ${numero} X ${Numero} = ${numero * Numero}`)
- numero++
+let numero = 5
+let numero2= 3
+for(let i = 1; i <= 10; i++){
+    console.log('a multi e ' + numero + ' X ' + i + ' = ' + (numero * i ) )
 }
