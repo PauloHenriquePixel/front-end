a14484522998c9c84e08432e1bd9a7484bc6d9cb
PauloHenriquePixel <paulohenriquealves4lavra@gmail.com>
2026-02-02 12:05:09 -0300

    back end em pythom

---
+++ Pythondecria/aula1.py
@@ -0,0 +1,86 @@
+nome = "paulo"
+idade = 18
+peso = 64.2
+trabalha = False
+Fazcursso = True
+
+print(type(nome))
+print(type(idade))
+print(type(peso))
+
+if trabalha:
+    print(False)
+elif Fazcursso:
+    print(True)
+else:
+    print(False)
+
+    VARIAVEIS
+nome = "Carlos"
+idade = 27
+peso = 54.5
+trabalha = False
+pensando = True
+
+forma mais atual com f-string
+print(f"Meu nome e {nome} e tenho {idade} anos")
+
+forma mais antiga
+print("Meu nome e {} e tenho {}anos ".format(nome, idade))
+
+
+TIPO DE DADOS
+print(type(nome)) # string
+print(type(idade)) # int
+print(type(peso)) # float 
+
+IF / ELSE / ELIF
+if(trabalha):
+    print("true")
+elif(pensando): 
+    print("vou pensar")
+else:
+    print("False")
+Caduh — Ontem às 20:08
+FUNÇÕES E SEUS PARAMETROS
+def ola(nome):
+    print(f"ola, {nome}")
+ola(nome="carlos")
+Caduh — Ontem às 20:52
+LIST / LISTA
+# # funciona
+🎒 list = uma mala
+Uma list é como uma mala/mochila onde você pode:
+colocar coisas dentro
+tirar coisas
+trocar coisas
+reorganizar
+abrir e ver o que tem dentro
+compras = ["arroz", 
+           "macarracoa", 
+           "feijao", 
+            "frango", 
+           "macaxeira"]
+print(compras)
+
+add valores
+append
+compras.append("cafe")
+print(compras)
+
+insert
+1 posição 2 valor
+compras.insert(3, "acucar")
+print(compras)
+
+
+remover
+compras.remove("acucar")
+print(compras)
+
+pop
+compras.pop()
+print(compras)
+passando parametro ele remove o valor
+compras.pop(0)
+print(compras)
